82b3f873f39a84307b6762a4f85d12868dc41199
Fabian <schenkfab@users.noreply.github.com>
2020-05-23 00:07:37 +0200

    Remove console log

--- src/auth/authGuard.js
+++ src/auth/authGuard.js
@@ -19,8 +19,6 @@ export const authGuard = (to, from, next) => {
         // make sure the user is already registered inside of the app itself with its this.$auth.user.sub
         const token = await authService.getTokenSilently()
 
-        console.log('token', authService.user)
-
         try {
           const { data } = await axios.post(
             POST_NON_DUB_USER(),
