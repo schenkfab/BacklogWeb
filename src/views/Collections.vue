553fa31d95b9bd29f42d6e3d21394f8f546eff3a
Fabian <schenkfab@users.noreply.github.com>
2020-05-28 07:27:39 +0200

    Add information text to feeds and collections

--- src/views/Collections.vue
+++ src/views/Collections.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="container mx-auto items-center justify-center" v-if="this.getUser.initialized">
-    <div class="text-left mb-8">
+    <div class="text-left mb-4">
       <h1 class="text-3xl text-purple-600">Collections</h1>
       <hr>
     </div>
+    <p class="mb-4">Everyone has by default one collection which is private. It is being created during registration and is called "My Collection". Feel free to head over to the feeds page and add some feeds to your "My Collection".<br>
+    In addition to that, Backlog allows users to create several private and public collections which can be shared across the platform.
+    </p>
     <div class="text-left mb-8">
       <h1 class="text-xl text-purple-400">Add Collection</h1>
     </div>
--- src/views/Feeds.vue
+++ src/views/Feeds.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="container mx-auto items-center justify-center" v-if="this.getUser.initialized">
-    <div class="text-center mb-8">
-      <h1 class="text-3xl">Feeds</h1>
+    <div class="text-left mb-4">
+      <h1 class="text-3xl text-purple-600">Feeds</h1>
       <hr>
     </div>
+    <p class="mb-4">Feeds are shared across all users and can be added by anyone who is registered. Once added, everyone can subscribe to them by adding a feed to one of their collections.<br>
+      As not all feeds are the same, there is a chance, that adding one may fail. If it fails, simply submit the error to us and we will make sure to improve backlog and add the feed ourselves.
+    </p>
+    <div class="text-left mb-8">
+      <h1 class="text-xl text-purple-400">Add Feed</h1>
+    </div>
     <add-feed></add-feed>
+    <div class="text-left mb-8">
+      <h1 class="text-xl text-purple-400">List of Feeds</h1>
+    </div>
     <div>
       <feeds-table :mycollections="getMyCollections" @subscribe="subscribe" @unsubscribe="unsubscribe" :feeds="this.getFeeds" :subscribed="this.getSubscribed"></feeds-table>
     </div>
