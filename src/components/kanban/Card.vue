07e70a2241f09290712800c3f00e2abee748e75f
Fabian <schenkfab@users.noreply.github.com>
2020-06-04 21:10:07 +0200

    Layout changes to the card
    - Add feed name to the card
    - Smaller logo
    - Change grey-ish color to purple

--- src/components/kanban/Card.vue
+++ src/components/kanban/Card.vue
@@ -2,9 +2,9 @@
     <li class="p-2 mb-3 bg-white shadow rounded-lg cursor-move" style="min-width: 300px;">
         <div class="flex justify-between items-center">
           <div class="flex items-center">
-            <img class="w-10 h-10 rounded-full" v-if="data.picture" :src="data.picture" :alt="data.name">
-            <img class="w-10 h-10 rounded-full" v-if="!data.picture" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Generic_Feed-icon.svg/1024px-Generic_Feed-icon.svg.png" :alt="data.name">
-            <p class="ml-2 text-gray-700 font-sans tracking-wide text-sm">{{data.name}}</p>
+            <img class="w-8 h-8 rounded-full" v-if="data.picture" :src="data.picture" :alt="data.name">
+            <img class="w-8 h-8 rounded-full" v-if="!data.picture" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Generic_Feed-icon.svg/1024px-Generic_Feed-icon.svg.png" :alt="data.name">
+            <p class="ml-2 text-gray-700 font-sans tracking-wide text-sm text-left">{{data.name}}</p>
           </div>
           <div class="flex">
             <button aria-label="Go to Link"
@@ -14,7 +14,7 @@
             </button>
           </div>
         </div>
-        <p class="text-gray-400 font-sans text-xs">{{(new Date(data.date)).toLocaleDateString()}}</p>
+        <p class="text-purple-600 font-sans text-xs">{{(new Date(data.date)).toLocaleDateString()}} - {{ getFeedNameShort(data.feed.name) }}</p>
     </li>
 </template>
 <script>
@@ -27,6 +27,15 @@ export default {
     data: {
       type: Object,
       default: () => ({})
+    }
+  },
+  methods: {
+    getFeedNameShort (feedName) {
+      if (feedName.length <= 30) {
+        return feedName
+      } else {
+        return `${feedName.substring(0, 30)}...`
+      }
     }
   }
 }
