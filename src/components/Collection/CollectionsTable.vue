59b45a3c779886c2f5dfb4edb0e96a82f76179a7
Fabian <schenkfab@users.noreply.github.com>
2020-06-11 12:21:48 +0200

    Remove add collection from the my collections view and extract unfollow and follow button

--- src/components/Collection/CollectionsTable.vue
+++ src/components/Collection/CollectionsTable.vue
@@ -10,16 +10,12 @@
       </thead>
       <tbody>
         <tr v-for="collection in this.collections" :key="collection.id">
-          <td class="border px-4 py-2"><button class="text-purple-600" @click="goToCollection(collection.id)">{{ getName(collection.name) }}</button></td>
+          <td class="border px-4 py-2"><button class="text-purple-600" @click="goToCollection(collection.id)">{{ getCollectionName(collection.id) }}</button></td>
           <td class="border px-4 py-2">{{ collection.description }}</td>
           <td class="border px-4 py-2">{{ getIsPrivate(collection.isPrivate) }}</td>
-          <td class="border px-4 py-2">
-            <button v-if="!alreadyFollowed(collection.id) && getName(collection.name) != 'My Collection'" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" @click="follow(collection.id)">
-              Follow
-            </button>
-            <button v-if="alreadyFollowed(collection.id) && getName(collection.name) != 'My Collection'" class="bg-white hover:bg-gray-100 text-red-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" @click="unfollow(collection.id)">
-              Unfollow
-            </button>
+          <td class="border py-2">
+            <follow :id="collection.id" v-if="!alreadyFollowed(collection.id) && getCollectionName(collection.id) != 'My Collection'"></follow>
+            <unfollow :id="collection.id" v-if="alreadyFollowed(collection.id) && getCollectionName(collection.id) != 'My Collection'"></unfollow>
           </td>
         </tr>
       </tbody>
@@ -28,8 +24,11 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import FollowButton from '@/components/Collection/FollowButton'
+import UnfollowButton from '@/components/Collection/UnfollowButton'
 
 export default {
+  components: { follow: FollowButton, unfollow: UnfollowButton },
   props: {
     collections: {
       type: Array,
@@ -41,24 +40,14 @@ export default {
     }
   },
   computed: {
-    ...mapGetters(['getUser'])
+    ...mapGetters(['getUser', 'getCollectionName'])
   },
   methods: {
     goToCollection (id) {
       this.$router.push('/collection/' + id).catch(err => { console.log(err) })
     },
-    getName (name) {
-      if (name === this.getUser.sub) {
-        return 'My Collection'
-      } else {
-        return name
-      }
-    },
     getIsPrivate (isPrivate) {
       return isPrivate ? 'Yes' : 'No'
-    },
-    follow: function (id) {
-      this.$emit('follow', id)
     },
     unfollow: function (collectionId) {
       this.$emit('unfollow', collectionId)
--- src/views/Collections.vue
+++ src/views/Collections.vue
@@ -8,33 +8,28 @@
     In addition to that, Backlog allows users to create several private and public collections which can be shared across the platform.
     </p>
     <div class="text-left mb-8">
-      <h1 class="text-xl text-purple-400">Add Collection</h1>
-    </div>
-    <add-collection></add-collection>
-    <div class="text-left mb-8">
       <h1 class="text-xl text-purple-400">My Collections</h1>
     </div>
     <div class="mb-8">
-      <collections-table @follow="follow" @unfollow="unfollow" :collections="this.getMyCollections" :followed="this.getFollowed()"></collections-table>
+      <collections-table :collections="this.getMyCollections" :followed="this.getFollowed()"></collections-table>
     </div>
     <div class="text-left mb-8">
       <h1 class="text-xl text-purple-400">All Collections</h1>
     </div>
     <div>
-      <collections-table @follow="follow" @unfollow="unfollow" :collections="this.getCollections" :followed="this.getFollowed()"></collections-table>
+      <collections-table :collections="this.getCollections" :followed="this.getFollowed()"></collections-table>
     </div>
 
   </div>
 </template>
 
 <script>
-import AddCollection from '@/components/Collection/AddCollection'
 import CollectionsTable from '@/components/Collection/CollectionsTable'
 import { mapGetters, mapActions, mapMutations } from 'vuex'
 
 export default {
   components: {
-    AddCollection, CollectionsTable
+    CollectionsTable
   },
   data: () => {
     return {}
@@ -51,17 +46,6 @@ export default {
         followed.push(x.collection.id)
       })
       return followed
-    },
-    async follow (collectionId) {
-      this.setLoading(true)
-      await this.addFollowsAsync(collectionId)
-      this.setLoading(false)
-    },
-    async unfollow (collectionId) {
-      this.setLoading(true)
-      var followId = this.getFollows.filter(o => o.collection.id === collectionId)[0].id
-      await this.unfollowAsync(followId)
-      this.setLoading(false)
     }
   },
   mounted: async function () {
