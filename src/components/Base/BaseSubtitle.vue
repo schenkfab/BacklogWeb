97e1a8236570e5cc03e5fd26199159fd37ff744a
Fabian <schenkfab@users.noreply.github.com>
2020-06-13 19:09:52 +0200

    Add Base components that are added everywhere

---
+++ src/components/Base/BaseSubtitle.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container max-w-5xl mx-auto m-8">
+    <h1 class="w-full my-2 text-3xl font-bold leading-tight text-center text-gray-800">
+      <slot />
+    </h1>
+    <div class="w-full mb-4">
+      <div class="h-1 mx-auto gradient w-48 opacity-25 my-0 py-0 rounded-t"></div>
+    </div>
+  </div>
+</template>
