b383aa69c97f6f32820e3d739feed8d436c3f748
Fabian <schenkfab@users.noreply.github.com>
2020-06-14 15:32:50 +0200

    Fix formating

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,12 @@
 <template>
   <nav class="flex items-center justify-between flex-wrap pl-4">
     <div class="flex items-center flex-shrink-0 mr-6">
-<svg class="h-8 fill-current inline" id="icon-bar-chart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                        fill="none" stroke="#2a2a31" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <line class="bar1" x1="12" y1="20" x2="12" y2="10"></line>
-                        <line class="bar2" x1="18" y1="20" x2="18" y2="4"></line>
-                        <line class="bar3" x1="6" y1="20" x2="6" y2="16"></line>
-                </svg>
-      <span class="font-semibold text-3xl tracking-tight pl-4 bg-nav">
- Backlog</span>
+      <svg class="h-8 fill-current inline" id="icon-bar-chart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#2a2a31" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <line class="bar1" x1="12" y1="20" x2="12" y2="10"></line>
+        <line class="bar2" x1="18" y1="20" x2="18" y2="4"></line>
+        <line class="bar3" x1="6" y1="20" x2="6" y2="16"></line>
+      </svg>
+      <span class="font-semibold text-3xl tracking-tight pl-4 bg-nav">Backlog</span>
     </div>
     <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto pt-1">
       <div class="lg:flex-grow">
