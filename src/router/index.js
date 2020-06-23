473089347aaded87ef77a039677ee74cc053c101
Fabian <schenkfab@users.noreply.github.com>
2020-06-23 11:26:38 +0200

    Fix error where about.vue was still being loaded inside of the router

--- src/router/index.js
+++ src/router/index.js
@@ -82,14 +82,6 @@ const routes = [
     props: true,
     component: Collection,
     beforeEnter: authGuard
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
